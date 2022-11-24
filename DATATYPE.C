f8dc3991110e22fe918c81e7391aee7b5f527d2e
varshan105 <logavarshinixd19@gmail.com>
2022-11-24 22:52:02 +0530

    c programs

---
+++ DATATYPE.C
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+ main()
+{
+    char a;
+    int b;
+    float c;
+    clrscr();
+    printf("enter a character:");
+    scanf("%c", &a);
+    printf("your input character value is:%c",a);
+    printf("\nenter a integer value:");
+    scanf("%d", &b);
+    printf("your input integer value is:%d",b);
+    printf("\nenter a float value:");
+    scanf(" %f", &c);
+    printf("your input float value is:%f",c);
+    printf("\ncompleted successfully");
+    getch();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ HELLOWOR.C
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+	printf("hello world");
+	printf("hi hello");
+	return 0;
+	}
